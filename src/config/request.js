22f5525d5b02456fc44741d436eaa5bb42665ad4
Lazizbek <oddiybolakay05@gmail.com>
2024-08-27 22:31:39 +0500

    end-cors

--- src/config/request.js
+++ src/config/request.js
@@ -1,3 +1,4 @@
 import axios from "axios";
 
-export const request = axios.create({ baseURL: "https://gipermarketapi.vercel.app" });
+// export const request = axios.create({ baseURL: "https://gipermarketapi.vercel.app" });
+export const request = axios.create({ baseURL: "http://localhost:3600" });
